f1bd40c75fee0be42fe878f852328731fbf1d63b
cpwallah <shivanshnegi2860@gmail.com>
2025-04-17 13:12:51 +0530

    set the client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/header";
+import { ReactQueryProvider } from "@/providers/tanstack/react-query-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ReactQueryProvider>
+        <Header/>
         {children}
+        </ReactQueryProvider>
       </body>
     </html>
   );
---
+++ src/lib/api.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
+  withCredentials: true,
+});
+
+export const logout = async () => {
+  const response = await api.get("/auth/logout");
+  return response.data;
+};
